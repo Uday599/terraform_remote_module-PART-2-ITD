1495981d65f8b58a4cb30049e3eb601f99ec67b6
Uday <70462083+Uday599@users.noreply.github.com>
2024-06-17 22:38:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,15 @@ module "remote-exec" {
   ec2_public_ip = module.create_ec2.ec2_public_ip
   ec2_pem_path = "./keys/terra_key.pem"
 }
+
+### Examples
+
+# select a specific tag
+module "vpc" {
+  source = "git::https://example.com/vpc.git?ref=v1.2.0"
+}
+
+# directly select a commit using its SHA-1 hash
+module "storage" {
+  source = "git::https://example.com/storage.git?ref=51d462976d84fdea54b47d80dcabbf680badcdb8"
+}
